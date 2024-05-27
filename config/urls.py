952fd025210180125791c91d2512cf044cc8e171
Taufeeq1996 <taufeeqk@techis.io>
2024-05-27 12:29:29 +0530

    updated urls.py

--- config/urls.py
+++ config/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from tweets.views import tweetListView, tweetAdd, tweetDelete, tweetEditView, tweetLikeAdd, tweetLikeSubtract
+from tweets.views import tweetListView, tweetAdd, tweetDelete, tweetEditView, tweetLikeAdd
 
 urlpatterns = [
     path('admin/', admin.site.urls),
